ce880df8f5615854caf6daa9bd71250fd639144e
Cali Castle <cali@calicastle.com>
2018-07-26 14:46:33 -0700

    Finish

--- PixabaySearch/Models/Image.swift
+++ PixabaySearch/Models/Image.swift
@@ -5,22 +5,28 @@ public struct Image {
     // MARK: - Properties
     
     /// Preview image url.
-    public var imageUrl: String?
+    public let imageUrl: String
     
     /// Full size image url.
-    public var fullSizeImageUrl: String?
+    public let fullSizeImageUrl: String
     
     /// Tags in string.
-    public var tags = ""
+    public let tags: String
+    
+    /// Image's title.
+    public let title: String
+    
+    /// Image's description.
+    public let description: String
     
     /// Computed url property.
     public var url: URL? {
-        return URL(string: imageUrl ?? "")
+        return URL(string: imageUrl)
     }
     
     /// Computed full size url property.
     public var fullUrl: URL? {
-        return URL(string: fullSizeImageUrl ?? "")
+        return URL(string: fullSizeImageUrl)
     }
     
     /// Quickly whip up an Image using Dictionary.
@@ -30,18 +36,25 @@ public struct Image {
     static public func make(attributes: [String: Any]) -> Image? {
         guard let imageUrl = attributes["webformatURL"] as? String,
             let fullSizeImageUrl = attributes["largeImageURL"] as? String,
-            let tags = attributes["tags"] as? String else { return nil }
+            let tags = attributes["tags"] as? String,
+            let user = attributes["user"] as? String,
+            let comments = attributes["comments"] as? Int,
+            let likes = attributes["likes"] as? Int,
+            let downloads = attributes["downloads"] as? Int
+        else { return nil }
         
-        let instance = Image(imageUrl: imageUrl, fullSizeImageUrl: fullSizeImageUrl, tags: tags)
+        let instance = Image(imageUrl: imageUrl, fullSizeImageUrl: fullSizeImageUrl, tags: tags, title: "Uploaded by: @\(user)", description: "\(comments) Comments, \(likes) Likes, \(downloads) Downloads")
         
         return instance
     }
     
     /// Initializer.
-    public init(imageUrl: String, fullSizeImageUrl: String, tags: String) {
+    public init(imageUrl: String, fullSizeImageUrl: String, tags: String, title: String, description: String) {
         self.imageUrl = imageUrl
         self.fullSizeImageUrl = fullSizeImageUrl
         self.tags = tags
+        self.title = title
+        self.description = description
     }
     
 }
